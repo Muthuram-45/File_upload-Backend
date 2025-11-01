614229b0edea86cab889023a03b1c5ece805b7c9
Muthuram-45 <muthuram921@gmail.com.com>
2025-11-01 12:43:14 +0530

    db update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import mysql from 'mysql2';
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
@@ -46,12 +47,15 @@ app.use(cors());
 // =============================
 // MySQL Connection
 // =============================
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 db.connect((err) => {
